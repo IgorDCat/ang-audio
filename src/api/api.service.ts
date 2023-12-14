dd902a7c233f55917f720ed8e51c6b38a511d4f8
IgorDCat <powerwolf891028@gmail.com>
2023-12-14 03:13:24 +0300

    changed audio page

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -6,9 +6,9 @@ import { Sound } from '../types/sound.type';
 })
 export class ApiService {
   soundsData: Sound[] = [
-    {id: '1', artist: 'artist 1', name: 'song 1', address: 'https://mp3melodii.ru/files_site_02/001/vertu_novyj_zvonok.mp3'},
-    {id: '2', artist: 'artist 2', name: 'song 2', address: 'https://mp3melodii.ru/files_site_02/001/krasivaya_melodiya_na_zvonok_i_like_chopin_djk.mp3'},
-    {id: '3', artist: 'artist 3', name: 'song 3', address: 'https://mp3melodii.ru/files_site_02/001/krasivyj_rington_vertu_trap.mp3'},
+    {id: '1', artist: 'sound 1', name: 'sound recording', address: 'https://mp3melodii.ru/files_site_02/001/vertu_novyj_zvonok.mp3'},
+    {id: '2', artist: 'sound 2', name: 'sound recording 2', address: 'https://mp3melodii.ru/files_site_02/001/krasivaya_melodiya_na_zvonok_i_like_chopin_djk.mp3'},
+    {id: '3', artist: 'sound 3', name: 'sound recording 3', address: 'https://mp3melodii.ru/files_site_02/001/krasivyj_rington_vertu_trap.mp3'},
   ];
 
   getSounds() {
